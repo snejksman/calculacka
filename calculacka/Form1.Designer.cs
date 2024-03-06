bb90f5cc899db8ace5751d1f0833e213d02fdc45
snejksman <ondrej.black@seznam.cz>
2024-03-06 15:54:42 +0100

    Add project files.

---
+++ calculacka/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace calculacka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btOperaceVysledek = new System.Windows.Forms.Button();
+            this.btClear = new System.Windows.Forms.Button();
+            this.btCislo0 = new System.Windows.Forms.Button();
+            this.btCislo9 = new System.Windows.Forms.Button();
+            this.btCislo8 = new System.Windows.Forms.Button();
+            this.btCislo7 = new System.Windows.Forms.Button();
+            this.btCislo6 = new System.Windows.Forms.Button();
+            this.btCislo5 = new System.Windows.Forms.Button();
+            this.btCislo4 = new System.Windows.Forms.Button();
+            this.btCislo3 = new System.Windows.Forms.Button();
+            this.btCislo2 = new System.Windows.Forms.Button();
+            this.btCislo1 = new System.Windows.Forms.Button();
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.btOperaceMinus = new System.Windows.Forms.Button();
+            this.btOperaceKrat = new System.Windows.Forms.Button();
+            this.btOperaceDeleno = new System.Windows.Forms.Button();
+            this.btOperacePlus = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btOperaceVysledek
+            // 
+            this.btOperaceVysledek.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.btOperaceVysledek.Location = new System.Drawing.Point(267, 343);
+            this.btOperaceVysledek.Margin = new System.Windows.Forms.Padding(4);
+            this.btOperaceVysledek.Name = "btOperaceVysledek";
+            this.btOperaceVysledek.Size = new System.Drawing.Size(119, 78);
+            this.btOperaceVysledek.TabIndex = 33;
+            this.btOperaceVysledek.Text = "=";
+            this.btOperaceVysledek.UseVisualStyleBackColor = true;
+            this.btOperaceVysledek.Click += new System.EventHandler(this.btOperace_Click);
+            // 
+            // btClear
+            // 
+            this.btClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.btClear.Location = new System.Drawing.Point(140, 343);
+            this.btClear.Margin = new System.Windows.Forms.Padding(4);
+            this.btClear.Name = "btClear";
+            this.btClear.Size = new System.Drawing.Size(119, 78);
+            this.btClear.TabIndex = 32;
+            this.btClear.Text = "C";
+            this.btClear.UseVisualStyleBackColor = true;
+            this.btClear.Click += new System.EventHandler(this.btOperace_Click);
+            // 
+            // btCislo0
+            // 
+            this.btCislo0.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.btCislo0.Location = new System.Drawing.Point(13, 343);
+            this.btCislo0.Margin = new System.Windows.Forms.Padding(4);
+            this.btCislo0.Name = "btCislo0";
+            this.btCislo0.Size = new System.Drawing.Size(119, 78);
+            this.btCislo0.TabIndex = 31;
+            this.btCislo0.Text = "0";
+            this.btCislo0.UseVisualStyleBackColor = true;
+            this.btCislo0.Click += new System.EventHandler(this.btCislo_Click);
+            // 
+            // btCislo9
+            // 
+            this.btCislo9.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.btCislo9.Location = new System.Drawing.Point(267, 85);
+            this.btCislo9.Margin = new System.Windows.Forms.Padding(4);
+            this.btCislo9.Name = "btCislo9";
+            this.btCislo9.Size = new System.Drawing.Size(119, 78);
+            this.btCislo9.TabIndex = 30;
+            this.btCislo9.Text = "9";
+            this.btCislo9.UseVisualStyleBackColor = true;
+            this.btCislo9.Click += new System.EventHandler(this.btCislo_Click);
+            // 
+            // btCislo8
+            // 
+            this.btCislo8.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.btCislo8.Location = new System.Drawing.Point(140, 85);
+            this.btCislo8.Margin = new System.Windows.Forms.Padding(4);
+            this.btCislo8.Name = "btCislo8";
+            this.btCislo8.Size = new System.Drawing.Size(119, 78);
+            this.btCislo8.TabIndex = 29;
+            this.btCislo8.Text = "8";
+            this.btCislo8.UseVisualStyleBackColor = true;
+            this.btCislo8.Click += new System.EventHandler(this.btCislo_Click);
+            // 
+            // btCislo7
+            // 
+            this.btCislo7.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.btCislo7.Location = new System.Drawing.Point(13, 85);
+            this.btCislo7.Margin = new System.Windows.Forms.Padding(4);
+            this.btCislo7.Name = "btCislo7";
+            this.btCislo7.Size = new System.Drawing.Size(119, 78);
+            this.btCislo7.TabIndex = 28;
+            this.btCislo7.Text = "7";
+            this.btCislo7.UseVisualStyleBackColor = true;
+            this.btCislo7.Click += new System.EventHandler(this.btCislo_Click);
+            // 
+            // btCislo6
+            // 
+            this.btCislo6.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.btCislo6.Location = new System.Drawing.Point(267, 171);
+            this.btCislo6.Margin = new System.Windows.Forms.Padding(4);
+            this.btCislo6.Name = "btCislo6";
+            this.btCislo6.Size = new System.Drawing.Size(119, 78);
+            this.btCislo6.TabIndex = 27;
+            this.btCislo6.Text = "6";
+            this.btCislo6.UseVisualStyleBackColor = true;
+            this.btCislo6.Click += new System.EventHandler(this.btCislo_Click);
+            // 
+            // btCislo5
+            // 
+            this.btCislo5.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.btCislo5.Location = new System.Drawing.Point(140, 171);
+            this.btCislo5.Margin = new System.Windows.Forms.Padding(4);
+            this.btCislo5.Name = "btCislo5";
+            this.btCislo5.Size = new System.Drawing.Size(119, 78);
+            this.btCislo5.TabIndex = 26;
+            this.btCislo5.Text = "5";
+            this.btCislo5.UseVisualStyleBackColor = true;
+            this.btCislo5.Click += new System.EventHandler(this.btCislo_Click);
+            // 
+            // btCislo4
+            // 
+            this.btCislo4.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.btCislo4.Location = new System.Drawing.Point(13, 171);
+            this.btCislo4.Margin = new System.Windows.Forms.Padding(4);
+            this.btCislo4.Name = "btCislo4";
+            this.btCislo4.Size = new System.Drawing.Size(119, 78);
+            this.btCislo4.TabIndex = 25;
+            this.btCislo4.Text = "4";
+            this.btCislo4.UseVisualStyleBackColor = true;
+            this.btCislo4.Click += new System.EventHandler(this.btCislo_Click);
+            // 
+            // btCislo3
+            // 
+            this.btCislo3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.btCislo3.Location = new System.Drawing.Point(267, 257);
+            this.btCislo3.Margin = new System.Windows.Forms.Padding(4);
+            this.btCislo3.Name = "btCislo3";
+            this.btCislo3.Size = new System.Drawing.Size(119, 78);
+            this.btCislo3.TabIndex = 24;
+            this.btCislo3.Text = "3";
+            this.btCislo3.UseVisualStyleBackColor = true;
+            this.btCislo3.Click += new System.EventHandler(this.btCislo_Click);
+            // 
+            // btCislo2
+            // 
+            this.btCislo2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.btCislo2.Location = new System.Drawing.Point(140, 257);
+            this.btCislo2.Margin = new System.Windows.Forms.Padding(4);
+            this.btCislo2.Name = "btCislo2";
+            this.btCislo2.Size = new System.Drawing.Size(119, 78);
+            this.btCislo2.TabIndex = 23;
+            this.btCislo2.Text = "2";
+            this.btCislo2.UseVisualStyleBackColor = true;
+            this.btCislo2.Click += new System.EventHandler(this.btCislo_Click);
+            // 
+            // btCislo1
+            // 
+            this.btCislo1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.btCislo1.Location = new System.Drawing.Point(13, 257);
+            this.btCislo1.Margin = new System.Windows.Forms.Padding(4);
+            this.btCislo1.Name = "btCislo1";
+            this.btCislo1.Size = new System.Drawing.Size(119, 78);
+            this.btCislo1.TabIndex = 22;
+            this.btCislo1.Text = "1";
+            this.btCislo1.UseVisualStyleBackColor = true;
+            this.btCislo1.Click += new System.EventHandler(this.btCislo_Click);
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Font = new System.Drawing.Font("Cambria", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtDisplay.Location = new System.Drawing.Point(13, 13);
+            this.txtDisplay.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.Size = new System.Drawing.Size(519, 64);
+            this.txtDisplay.TabIndex = 21;
+            this.txtDisplay.Text = "0";
+            this.txtDisplay.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btOperaceMinus
+            // 
+            this.btOperaceMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.btOperaceMinus.Location = new System.Drawing.Point(413, 171);
+            this.btOperaceMinus.Margin = new System.Windows.Forms.Padding(4);
+            this.btOperaceMinus.Name = "btOperaceMinus";
+            this.btOperaceMinus.Size = new System.Drawing.Size(119, 78);
+            this.btOperaceMinus.TabIndex = 20;
+            this.btOperaceMinus.Text = "-";
+            this.btOperaceMinus.UseVisualStyleBackColor = true;
+            this.btOperaceMinus.Click += new System.EventHandler(this.btOperace_Click);
+            // 
+            // btOperaceKrat
+            // 
+            this.btOperaceKrat.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.btOperaceKrat.Location = new System.Drawing.Point(412, 257);
+            this.btOperaceKrat.Margin = new System.Windows.Forms.Padding(4);
+            this.btOperaceKrat.Name = "btOperaceKrat";
+            this.btOperaceKrat.Size = new System.Drawing.Size(119, 78);
+            this.btOperaceKrat.TabIndex = 19;
+            this.btOperaceKrat.Text = "*";
+            this.btOperaceKrat.UseVisualStyleBackColor = true;
+            this.btOperaceKrat.Click += new System.EventHandler(this.btOperace_Click);
+            // 
+            // btOperaceDeleno
+            // 
+            this.btOperaceDeleno.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.btOperaceDeleno.Location = new System.Drawing.Point(412, 343);
+            this.btOperaceDeleno.Margin = new System.Windows.Forms.Padding(4);
+            this.btOperaceDeleno.Name = "btOperaceDeleno";
+            this.btOperaceDeleno.Size = new System.Drawing.Size(119, 78);
+            this.btOperaceDeleno.TabIndex = 18;
+            this.btOperaceDeleno.Text = "/";
+            this.btOperaceDeleno.UseVisualStyleBackColor = true;
+            this.btOperaceDeleno.Click += new System.EventHandler(this.btOperace_Click);
+            // 
+            // btOperacePlus
+            // 
+            this.btOperacePlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.btOperacePlus.Location = new System.Drawing.Point(413, 85);
+            this.btOperacePlus.Margin = new System.Windows.Forms.Padding(4);
+            this.btOperacePlus.Name = "btOperacePlus";
+            this.btOperacePlus.Size = new System.Drawing.Size(119, 78);
+            this.btOperacePlus.TabIndex = 17;
+            this.btOperacePlus.Text = "+";
+            this.btOperacePlus.UseVisualStyleBackColor = true;
+            this.btOperacePlus.Click += new System.EventHandler(this.btOperace_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 450);
+            this.Controls.Add(this.btOperaceVysledek);
+            this.Controls.Add(this.btClear);
+            this.Controls.Add(this.btCislo0);
+            this.Controls.Add(this.btCislo9);
+            this.Controls.Add(this.btCislo8);
+            this.Controls.Add(this.btCislo7);
+            this.Controls.Add(this.btCislo6);
+            this.Controls.Add(this.btCislo5);
+            this.Controls.Add(this.btCislo4);
+            this.Controls.Add(this.btCislo3);
+            this.Controls.Add(this.btCislo2);
+            this.Controls.Add(this.btCislo1);
+            this.Controls.Add(this.txtDisplay);
+            this.Controls.Add(this.btOperaceMinus);
+            this.Controls.Add(this.btOperaceKrat);
+            this.Controls.Add(this.btOperaceDeleno);
+            this.Controls.Add(this.btOperacePlus);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btOperaceVysledek;
+        private System.Windows.Forms.Button btClear;
+        private System.Windows.Forms.Button btCislo0;
+        private System.Windows.Forms.Button btCislo9;
+        private System.Windows.Forms.Button btCislo8;
+        private System.Windows.Forms.Button btCislo7;
+        private System.Windows.Forms.Button btCislo6;
+        private System.Windows.Forms.Button btCislo5;
+        private System.Windows.Forms.Button btCislo4;
+        private System.Windows.Forms.Button btCislo3;
+        private System.Windows.Forms.Button btCislo2;
+        private System.Windows.Forms.Button btCislo1;
+        private System.Windows.Forms.TextBox txtDisplay;
+        private System.Windows.Forms.Button btOperaceMinus;
+        private System.Windows.Forms.Button btOperaceKrat;
+        private System.Windows.Forms.Button btOperaceDeleno;
+        private System.Windows.Forms.Button btOperacePlus;
+    }
+}
+
---
+++ calculacka/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculacka
+{
+    public partial class Form1 : Form
+    {
+        // typ operace
+        enum enOperace
+        {
+            Plus,
+            Minus,
+            Krat,
+            Deleno,
+            Clear,
+            Rovno
+        }
+        // promenne
+        float mflCislo1, mflCislo2, mflVysledek;
+
+        enOperace menAktOperace;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // smazani vseho na zacatku
+
+            VseVymazat();
+        }
+        //
+        // zapsani dalsiho cisla
+        //
+        private void btOperace_Click(object sender, EventArgs e)
+        {
+            //tlacitko ktere zavola funkci
+            Button lobjTlacitko = (Button)sender;
+
+            switch (lobjTlacitko.Text)
+            {
+                case "+":
+                    mflCislo2 = Convert.ToSingle(txtDisplay.Text);
+                    menAktOperace = enOperace.Plus;
+                    txtDisplay.Text = "0";
+                    break;
+
+                case "-":
+                    mflCislo2 = Convert.ToSingle(txtDisplay.Text);
+                    menAktOperace = enOperace.Minus;
+                    txtDisplay.Text = "0";
+                    break;
+
+                case "*":
+
+                    mflCislo2 = Convert.ToSingle(txtDisplay.Text);
+                    menAktOperace = enOperace.Krat;
+                    txtDisplay.Text = "0";
+                    break;
+
+                case "/":
+                    mflCislo2 = Convert.ToSingle(txtDisplay.Text);
+                    menAktOperace = enOperace.Deleno;
+                    txtDisplay.Text = "0";
+                    break;
+                case "=":
+                    mflCislo1 = mflCislo2;
+                    mflCislo2 = Convert.ToSingle(txtDisplay.Text);
+                    if (menAktOperace == enOperace.Plus)
+                    {
+                        mflVysledek = mflCislo1 + mflCislo2;
+                    }
+                    else if (menAktOperace == enOperace.Minus)
+                    {
+                        mflVysledek = mflCislo1 - mflCislo2;
+                    }
+                    else if (menAktOperace == enOperace.Krat)
+                    {
+                        mflVysledek = mflCislo1 * mflCislo2;
+                    }
+                    else if (menAktOperace == enOperace.Deleno)
+                    {
+                        mflVysledek = mflCislo1 / mflCislo2;
+                    }
+                    txtDisplay.Text = mflVysledek.ToString();
+                    break;
+            }
+
+        }
+        //
+        // zapsani operace
+        //
+        private void btCislo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //smazani nuly
+                if (txtDisplay.Text == "0") txtDisplay.Text = "";
+
+                //pridat dalsi cislo
+
+                txtDisplay.Text = txtDisplay.Text + ((Button)sender).Text;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("asi to nefunguje :(");
+            }
+        }
+        private void VseVymazat()
+        {
+            //smazat uvodni nulu
+
+            txtDisplay.Text = "0";
+
+            // inicializace promenne
+
+            mflCislo1 = mflCislo2 = mflVysledek = 0;
+            menAktOperace = enOperace.Clear;
+
+        }
+    }
+}
